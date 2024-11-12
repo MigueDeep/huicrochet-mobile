ff8fd0ae6654c9932f01c0b1e86c42cba849fc8d
Joviicam Racoon <joviicam9@gmail.com>
2024-11-12 12:29:25 -0600

    [FIX] Conflic with develop

--- lib/modules/shopping-cart/shoppingcart_screen.dart
+++ lib/modules/shopping-cart/shoppingcart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:huicrochet_mobile/widgets/product/product_added_to_cart.dart';
 import 'package:huicrochet_mobile/widgets/general/general_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ShoppingcartScreen extends StatefulWidget {
   const ShoppingcartScreen({super.key});
