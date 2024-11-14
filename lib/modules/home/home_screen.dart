a1c22531b8dda3dcf887b98622146c76d0505ae7
CaroJimenez <105475884+CaroJimenez@users.noreply.github.com>
2024-11-14 17:02:50 -0600

    [feat] new products and irregular images fixed

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:huicrochet_mobile/config/dio_client.dart';
+import 'package:huicrochet_mobile/config/global_variables.dart';
 import 'package:huicrochet_mobile/widgets/product/product_card.dart';
 import 'package:huicrochet_mobile/widgets/general/app_bar.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,15 +15,73 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, Object>> products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getProducts();
+  }
+
+  Future<void> _getProducts() async {
+    final dioClient = DioClient(context);
+
+    try {
+      final response = await dioClient.dio.get('/product/getTop10');
+
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.toString());
+
+        final List<dynamic> productData = jsonData['data'];
+        setState(() {
+          products = productData.map((product) {
+            final name = product['productName'] as String;
+            final productId = product['id'] as String;
+            final price = product['price'].toString();
+            final imageUri = (product['items'] != null &&
+                    product['items'].isNotEmpty &&
+                    product['items'][0]['images'] != null &&
+                    product['items'][0]['images'].isNotEmpty)
+                ? product['items'][0]['images'][0]['imageUri'] as String
+                : 'assets/logo.png';
+            final imageName = imageUri.split('/').last; // Imagen por defecto
+            final uriNetwork = 'http://${ip}:8080/$imageName';
+            return {
+              'name': name,
+              'price': price,
+              'imageUri': uriNetwork,
+              'productId': productId
+            };
+          }).toList();
+        });
+      } else {
+        print('Error al obtener productos ${response.statusCode}');
+      }
+    } on DioException catch (e) {
+      String errorMessage = 'Error al obtener productos';
+      if (e.response != null && e.response?.data != null) {
+        try {
+          final errorData = jsonDecode(e.response!.data);
+          errorMessage = errorData['message'] ?? errorMessage;
+        } catch (_) {
+          errorMessage = e.message ?? 'Error desconocido';
+        }
+      }
+      print('Error de red $errorMessage');
+    } catch (e) {
+      print('Error desconocido ${e.toString()}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Center( 
+        child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center, 
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
@@ -100,24 +164,27 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               SizedBox(height: 10),
-              // SizedBox(
-              //   width: MediaQuery.of(context).size.width * 0.9,
-              //   child: SingleChildScrollView(
-              //     scrollDirection: Axis.horizontal,
-              //     child: Row(
-              //       children: ProductsScreen.products.map((product) {
-              //         return productCard(
-              //           product['name']!, // Nombre del producto
-              //           product['image']!, // URL de la imagen del producto
-              //           product['price']!, 
-              //           product['id']!,// Precio del producto
-              //           context,
-              //         );
-              //       }).toList(),
-              //     ),
-              //   ),
-              // ),
-              SizedBox(height: 20),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: products.map((product) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: productCard(
+                          product['name'] as String,
+                          product['imageUri'] as String,
+                          product['price'].toString(),
+                          product['productId'].toString(),
+                          context,
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 child: SizedBox(
@@ -148,23 +215,26 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               SizedBox(height: 10),
-              // SizedBox(
-              //   width: MediaQuery.of(context).size.width * 0.9,
-              //   child: SingleChildScrollView(
-              //     scrollDirection: Axis.horizontal,
-              //     child: Row(
-              //       children: ProductsScreen.products.map((product) {
-              //         return productCard(
-              //           product['name']!, // Nombre del producto
-              //           product['image']!, // URL de la imagen del producto
-              //           product['price']!,
-              //           product['id']!, // Precio del producto
-              //           context,
-              //         );
-              //       }).toList(),
-              //     ),
-              //   ),
-              // ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: products.map((product) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: productCard(
+                          product['name'] as String,
+                          product['imageUri'] as String,
+                          product['price'].toString(),
+                          product['productId'].toString(),
+                          context,
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
