57a5f697db53c574ba5e82cef79c3f3f1b3bd37a
CaroJimenez <105475884+CaroJimenez@users.noreply.github.com>
2024-11-21 23:34:28 -0600

    [fix] get product by id finished

--- lib/modules/product/providers/product_details_provider.dart
+++ lib/modules/product/providers/product_details_provider.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:huicrochet_mobile/config/dio_client.dart';
 import 'package:huicrochet_mobile/config/global_variables.dart';
-import 'package:huicrochet_mobile/modules/product/entities/product_category.dart';
 
 class ProductDetailsProvider with ChangeNotifier {
   List<Map<String, dynamic>> _productItems = [];
@@ -12,61 +11,86 @@ class ProductDetailsProvider with ChangeNotifier {
   List<Map<String, dynamic>> get products => _productItems;
 
   Future<void> fetchProductDetails(
-  BuildContext context, {
-  bool forceRefresh = false,
-  String endpoint = "/item/getItemsByProductId/",
-  required String id,
-}) async {
-  if (_productItems.isNotEmpty && !forceRefresh) return;
-  _isLoading = true;
-  notifyListeners();
-  final dioClient = DioClient(context);
+    BuildContext context, {
+    bool forceRefresh = false,
+    String endpoint = "/item/getItemsByProductId/",
+    required String id,
+  }) async {
+    if (_productItems.isNotEmpty &&
+        (!forceRefresh || _productItems[0]['productId'] == id)) {
+      return;
+    }
 
-  try {
-    final response = await dioClient.dio.get(endpoint + id);
-    if (response.statusCode == 200) {
-      final jsonData = jsonDecode(response.toString());
-      final List<dynamic> productData = jsonData['data'];
+    clearProductDetails();
+    _isLoading = true;
+    notifyListeners();
 
-      _productItems = productData.map((product) {
-        final name = product['product']['productName'] as String;
-        final productId = product['product']['id'] as String;
-        final price = product['product']['price'].toString();
-        
-        // Verificar si las categorías y las imágenes existen y no son nulas
-        final categories = (product['product']['categories'] as List<dynamic>?) ?? [];
-        final images = (product['images'] as List<dynamic>?) ?? [];
-        final color = product['color']['colorCod'] as String;
+    final dioClient = DioClient(context);
 
-        // Manejo de imágenes
-        final imageUris = images.map((image) {
-          return 'http://$ip:8080/${image['imageUri']?.split('/').last ?? 'default_image.png'}';
+    try {
+      final response = await dioClient.dio.get(endpoint + id);
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.toString());
+        final List<dynamic> productData = jsonData['data'];
+
+        _productItems = productData.map((product) {
+          // Datos básicos del producto
+          final itemId = product['id'] as String;
+          final name = product['product']['productName'] as String;
+          final description = product['product']['description'] as String;
+          final productId = product['product']['id'] as String;
+          final price = product['product']['price'].toString();
+
+          // Categorías
+          final categories =
+              (product['product']['categories'] as List<dynamic>?) ?? [];
+          final categoryNames = categories.map((category) {
+            return category['name'] as String;
+          }).toList();
+
+          // Imágenes
+          final images = (product['images'] as List<dynamic>?) ?? [];
+          final imageUris = images.map((image) {
+            return 'http://$ip:8080/${image['imageUri']?.split('/').last ?? 'default_image.png'}';
+          }).toList();
+
+          // Colores
+         final List<Map<String, dynamic>> allItems = productData.cast<Map<String, dynamic>>();
+final colors = allItems
+    .where((item) =>
+        item['product']['id'] == productId && item['color'] != null)
+    .map((item) => item['color']['colorCod'] as String)
+    .toSet() // Eliminar colores duplicados
+    .toList();
+
+
+          return {
+            'name': name,
+            'description': description,
+            'price': price,
+            'imageUris': imageUris,
+            'productId': productId,
+            'colors': colors, // Aquí tienes la lista de colores
+            'categoryNames': categoryNames,
+            'itemId': itemId,
+          };
         }).toList();
 
-        // Manejo de categorías
-        final categoryNames = categories.map((category) {
-          return category['name'] as String;
-        }).toList();
-
-        return {
-          'name': name,
-          'price': price,
-          'imageUris': imageUris,  // Guardar todas las URIs de las imágenes
-          'productId': productId,
-          'color': color,
-          'categoryNames': categoryNames  // Guardar todos los nombres de las categorías
-        };
-      }).toList();
-
+        _isLoading = false;
+        notifyListeners();
+      } else {
+        throw Exception('Error al obtener producto');
+      }
+    } catch (e) {
+      print('Error desconocido ${e.toString()}');
       _isLoading = false;
       notifyListeners();
-    } else {
-      throw Exception('Error al obtener producto');
+      throw Exception('Error desconocido');
     }
-  } catch (e) {
-    print('Error desconocido ${e.toString()}');
-    throw Exception('Error desconocido');
   }
-}
 
+  void clearProductDetails() {
+    _productItems = [];
+    notifyListeners();
+  }
 }
--- lib/modules/product/screens/productDetail_screen.dart
+++ lib/modules/product/screens/productDetail_screen.dart
@@ -1,13 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:huicrochet_mobile/modules/product/entities/product_category.dart';
+import 'package:huicrochet_mobile/config/dio_client.dart';
+import 'package:huicrochet_mobile/config/error_state.dart';
+import 'package:huicrochet_mobile/config/global_variables.dart';
 import 'package:huicrochet_mobile/modules/product/providers/product_details_provider.dart';
+import 'package:huicrochet_mobile/widgets/general/loader.dart';
 import 'package:huicrochet_mobile/widgets/product/product_detail_loading.dart';
 import 'package:huicrochet_mobile/widgets/product/select_colors.dart';
 import 'package:huicrochet_mobile/widgets/product/user_comment.dart';
-import 'package:huicrochet_mobile/widgets/general/general_button.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/material.dart' as flutter_ui;
 
 class ProductDetail extends StatefulWidget {
   const ProductDetail({super.key});
@@ -17,26 +19,123 @@ class ProductDetail extends StatefulWidget {
 }
 
 class _ProductDetailState extends State<ProductDetail> {
+  final LoaderController _loaderController = LoaderController();
   int _selectedIndex = 0;
+  String _selectedItem = '';
+  int _quantity = 0;
+  bool _isAdded = false;
+
+  void _incrementQuantity() {
+    setState(() {
+      _quantity++;
+    });
+  }
+
+  void _decrementQuantity() {
+    if (_quantity > 0) {
+      setState(() {
+        _quantity--;
+      });
+    }
+  }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
     if (args != null) {
       final productId = args['productId'] ?? '';
-      // Llamar la función de fetchProductDetails solo una vez cuando el ID cambia
       Provider.of<ProductDetailsProvider>(context, listen: false)
           .fetchProductDetails(context, id: productId);
     }
   }
+Future<void> checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getString('token') == null) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('No tienes sesión iniciada'),
+            content: const Text('¿Quieres iniciar sesión?'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Cancelar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: const Text('Iniciar sesión'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  Navigator.pushReplacementNamed(context, '/login');
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+     addToShoppingCart();
+    }
+  }
+   Future<void> addToShoppingCart() async {
+    _loaderController.show(context);
+    final dio = DioClient(context).dio;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    try {
+      final response = await dio.post(
+        '/shopping-cart/${prefs.getString('shoppingCartId')}',
+        data: {
+          'itemId': _selectedItem,
+          'quantity': _quantity,
+        },
+      );
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        setState(() {
+          _isAdded = true;
+        });
+        _loaderController.hide();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Producto agregado al carrito'),
+            backgroundColor: Colors.blue,
+          ),
+        );
+         await Future.delayed(const Duration(seconds: 2));
+        setState(() {
+          _isAdded = false;
+        });
+      }
+    } catch (e) {
+      final errorState = Provider.of<ErrorState>(context, listen: false);
+
+      if (e is DioException) {
+        if (e.response?.statusCode == 400) {
+          String errorMessage =
+              e.response?.data['message'] ?? 'Error desconocido';
+          errorState.setError(errorMessage);
+        } else {
+          errorState.setError('Error de conexión');
+        }
+      } else {
+        errorState.setError('Error inesperado: $e');
+      }
+      _loaderController.hide();
+
+      errorState.showErrorDialog(context);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Consumer<ProductDetailsProvider>(
       builder: (context, provider, child) {
         if (provider.isLoading) {
-          return Center(child: CircularProgressIndicator());
+          return const ProductDetailLoading();
         }
 
         if (provider.products.isEmpty) {
@@ -44,50 +143,282 @@ class _ProductDetailState extends State<ProductDetail> {
         }
 
         var productData = provider.products[0];
-
-        return Center(
-          child: Column(
-            children: [
-              Text(productData['name']),  // Mostrar el nombre del producto
-              Text(productData['price']), // Mostrar el precio del producto
-              // Mostrar todas las imágenes
-              Expanded(
-                child: ListView.builder(
-                  itemCount: productData['imageUris'].length,
-                  itemBuilder: (context, index) {
-                    var imageUri = productData['imageUris'][index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.network(
-                          imageUri,
-                          width: 320,
-                          height: 400,
-                          fit: BoxFit.cover,
-                          errorBuilder: (context, error, stackTrace) {
-                            return Image.asset(
-                              'assets/snoopyAzul.jpg',
-                              width: 320,
-                              height: 400,
-                              fit: BoxFit.cover,
-                            );
-                          },
+        var itemData = provider.products;
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+          ),
+          body: SingleChildScrollView(
+            child: Center(
+              child: Container(
+                  width: 350,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            if (itemData.isNotEmpty &&
+                                itemData[_selectedIndex] != null)
+                              ...itemData[_selectedIndex]['imageUris']
+                                  .map<Widget>((imageUri) {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 4.0),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(20),
+                                    child: Image.network(
+                                      imageUri ??
+                                          '', // Aquí usamos el valor mapeado de imageUris
+                                      width: 320,
+                                      height: 400,
+                                      fit: BoxFit.cover,
+                                      errorBuilder:
+                                          (context, error, stackTrace) {
+                                        return Image.asset(
+                                          'assets/snoopyAzul.jpg',
+                                          width: 320,
+                                          height: 400,
+                                          fit: BoxFit.cover,
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                );
+                              }).toList()
+                            else
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 40),
+                                child: Text(
+                                  'Imagen no disponible',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.grey),
+                                ),
+                              ),
+                          ],
                         ),
                       ),
-                    );
-                  },
-                ),
-              ),
-
-              // Mostrar categorías
-              Text('Categorías:'),
-              for (var category in productData['categoryNames'])
-                Text(' - $category'),
-
-              // Mostrar color
-              Text('Color: ${productData['color']}'),
-            ],
+                      const SizedBox(height: 10),
+                      Icon(
+                        Icons.maximize,
+                        color: Colors.grey,
+                        size: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            productData['name'],
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Row(children: [
+                            Text(
+                              '4.5',
+                              style: const TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 16,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            Icon(
+                              Icons.star_half,
+                              color: Colors.yellow,
+                            ),
+                          ]),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        width: double.infinity,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '\$${productData['price']}',
+                              style: const TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              productData['description'],
+                              style: const TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.category,
+                                  color: Colors.black,
+                                ),
+                                Text(
+                                  productData['categoryNames'][0],
+                                  style: const TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              'Colores',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            ColorSelector(
+                              colorCodes: itemData[_selectedIndex]['colors'],
+                              onColorSelected: (index) {
+                                setState(() {
+                                  _selectedIndex = index;
+                                  _selectedItem =
+                                      itemData[_selectedIndex]['itemId'];
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                                border: Border.all(color: Colors.grey[200]!)),
+                            child: IconButton(
+                              onPressed: _decrementQuantity,
+                              icon: Icon(Icons.remove),
+                            ),
+                          ),
+                           Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Text(
+                              '$_quantity',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                                border: Border.all(color: Colors.grey[200]!)),
+                            child: IconButton(
+                              onPressed: _incrementQuantity,
+                              icon: Icon(Icons.add),
+                            ),
+                          ),
+                        ],
+                      ),
+                       SizedBox(height: 10),
+                        SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            backgroundColor:
+                                _isAdded ? Colors.green : colors['violet'],
+                          ),
+                          onPressed: _quantity > 0 && !_isAdded
+                              ? () {
+                                  if (_selectedItem == '') {
+                                    setState(() {
+                                      _selectedItem = _selectedItem =itemData[0]['itemId'];
+                                    });
+                                  }
+                                  checkLoginStatus();
+                                }
+                              : null,
+                          child: Row(
+                            key: const ValueKey('animated_icon'),
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              AnimatedSwitcher(
+                                duration: const Duration(milliseconds: 300),
+                                child: _isAdded
+                                    ? Icon(Icons.check,
+                                        key: const ValueKey('check_icon'),
+                                        color: Colors.white)
+                                    : Icon(Icons.shopping_cart,
+                                        key: const ValueKey('cart_icon'),
+                                        color: Colors.white),
+                              ),
+                              const SizedBox(width: 8),
+                              Text(
+                                _isAdded
+                                    ? 'Agregado al carrito'
+                                    : 'Agregar al carrito',
+                                style: const TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 16,
+                                    color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Comentarios (3)',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 16,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            CommentWidget(
+                              username: 'Usuario123',
+                              date: '20 de octubre, 2024',
+                              description:
+                                  '¡Me encanta este producto! Es de excelente calidad.',
+                              rating: 4.5,
+                            ),
+                            CommentWidget(
+                              username: 'MariaLopez',
+                              date: '19 de octubre, 2024',
+                              description:
+                                  'Muy bueno, pero podría mejorar en algunos aspectos.',
+                              rating: 3.0,
+                            ),
+                          ],
+                        ),)
+                    ],
+                  )),
+            ),
           ),
         );
       },
